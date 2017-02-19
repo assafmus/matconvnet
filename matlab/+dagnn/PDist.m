5a345d5d895183dedc30455d68af9641567a582a
Assaf <assaf@server2.brodmann17.com>
2017-02-19 06:19:42 -0600

    matconvnet

--- matlab/+dagnn/PDist.m
+++ matlab/+dagnn/PDist.m
@@ -26,7 +26,7 @@ classdef PDist < dagnn.ElementWise
         function outputs = forward(obj, inputs, params)
             outputs{1} = vl_nnpdist(inputs{1}, inputs{2}, obj.p, 'noRoot', obj.noRoot, 'epsilon', obj.epsilon, 'aggregate', obj.aggregate, obj.opts{:}) ;
             n = obj.numAveraged ;
-            m = n + size(inputs{1},4) ;
+            m = n + size(obj.net.getVar('input').value,4) ;
             obj.average = (n * obj.average + gather(outputs{1})) / m ;
             obj.numAveraged = m ;
         end
